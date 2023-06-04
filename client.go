da5b23cca469254d0f5c66383f1a36dace1b04fb
shynome <shynome@gmail.com>
2023-06-04 11:02:36 +0800

    正确实现 AuthStore

--- client.go
+++ client.go
@@ -8,6 +8,8 @@ import (
 
 type Client struct {
 	Endpoint string
+
+	authStore *base.Service
 }
 
 func New(endpoint string) *Client {
@@ -17,11 +19,27 @@ func New(endpoint string) *Client {
 }
 
 func (c *Client) Health() *health.Service {
-	bs := base.New(c.Endpoint)
+	bs := c.getBS()
 	return health.New(bs)
 }
 
+func (c *Client) SetAuthStore(r *record.Service[record.UserBase]) {
+	c.authStore = r.Service.Service
+}
+
+func (c *Client) getBS() *base.Service {
+	if c.authStore != nil {
+		return c.authStore
+	}
+	return base.New(c.Endpoint)
+}
+
 func NewCollection[T any](c *Client, collection string) *record.Service[T] {
-	bs := base.New(c.Endpoint)
+	bs := c.getBS()
 	return record.New[T](bs, collection)
+}
+
+func NewAdmin[T any](c *Client) *record.Service[T] {
+	bs := c.getBS()
+	return record.New[T](bs, "-admins")
 }
